c7db8bf00c3df8daef1ed773e1c90d067c00399e
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2022-12-07 15:45:19 +0700

    make method stack and declare the variable

--- Program.cs
+++ Program.cs
@@ -11,6 +11,13 @@ namespace Exe4_141
         private string[] adri;
         private decimal top;
         private decimal max;
+
+        public Stack(int size)
+        {
+            adri = new string[size];
+            top = -1;
+            max = size;
+        }
     }
     internal class Program
     {
